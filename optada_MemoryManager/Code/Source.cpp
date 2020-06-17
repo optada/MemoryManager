dbf83297c36171e13182b64fbb35fde923a15613
optada <druzhinin_v_d@ukr.net>
2020-06-17 09:09:55 +0300

    Source has been updated

--- optada_MemoryManager/Code/Source.cpp
+++ optada_MemoryManager/Code/Source.cpp
@@ -16,28 +16,23 @@ void testFor_OPTadaC_MemoryCells_StaticCyclicBuffer()
 {
 	bool del = false;
 	OPTadaC_MemoryCells_StaticCyclicBuffer Buffer = OPTadaC_MemoryCells_StaticCyclicBuffer(5, del);
-
 	cout << endl << del;
 	cout << endl << Buffer.Get_AllCapturedMemory();
-	
+
 	void* element1 = nullptr;
 	void* element2 = nullptr;
 	void* element3 = nullptr;
 	void* element4 = nullptr;
 	void* element5 = nullptr;
 	void* element6 = nullptr;
-
 	element1 = Buffer.Get_Element();
 	element2 = Buffer.Get_Element();
 	element3 = Buffer.Get_Element();
 	element4 = Buffer.Get_Element();
 	element5 = Buffer.Get_Element();
 	element6 = Buffer.Get_Element();
-
 	Buffer.Return_Element((OPTadaS_MemoryCell_Element*)element2);
-
 	element2 = Buffer.Get_Element();
-
 	Buffer.Return_Element((OPTadaS_MemoryCell_Element*)element1);
 	Buffer.Return_Element((OPTadaS_MemoryCell_Element*)element2);
 	Buffer.Return_Element((OPTadaS_MemoryCell_Element*)element3);
@@ -54,76 +49,66 @@ void testFor_OPTadaC_SimpleMemoryBuffer()
 {
 	bool initDoneWithNoErrors_ = false;
 	OPTadaC_SimpleMemoryBuffer Buffer = OPTadaC_SimpleMemoryBuffer(10, 5, 2, initDoneWithNoErrors_);
-
 	cout << "initDoneWithNoErrors_ - " << initDoneWithNoErrors_ << endl;
 	cout << "test buffer - " << Buffer.TestBuffer() << endl;
 	cout << "All locked memory :" << Buffer.Get_AllModulesLockedMemory() << endl;
 	cout << "Memory size :" << Buffer.Get_BufferMemorySize() << endl;
 	cout << "Locked memory :" << Buffer.Get_LockedMemory() << endl;
-
 	void* pok1 = nullptr;
 	void* pok2 = nullptr;
 	void* pok3 = nullptr;
 	void* pok4 = nullptr;
 	void* pok5 = nullptr;
 	void* pok6 = nullptr;
-
 	// incorrect or specifick input
 	pok1 = Buffer.GetMemory(-1); // ! unsigned parameter
 	pok1 = Buffer.GetMemory(1000000); // fragmetation +0 (2b - fragmet cell)
 	pok1 = Buffer.GetMemory(1000001); // fragmetation +1 (2b - fragmet cell)
-	
+
 	// incorrect reff
 	int a;
 	pok1 = &a;
 	Buffer.ReturnMemory(pok1);
 	pok1 = nullptr;
 	Buffer.ReturnMemory(pok1);
-
 	// try get memory errors
 	pok1 = Buffer.GetMemory(8);
 	// low memory
 	pok2 = Buffer.GetMemory(3); // we have 2 now
 	// have no enaght memory
 	pok3 = Buffer.GetMemory(11); // we have 10 max
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
-
 	// fragmentaion mamory error +
 	pok1 = Buffer.GetMemory(4); // XX XX 00 00 00
 	pok2 = Buffer.GetMemory(2); // XX XX XX 00 00
 	pok3 = Buffer.GetMemory(4); // XX XX XX XX XX
 	Buffer.ReturnMemory(pok1); // 00 00 XX XX XX
-	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00 
+	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00
 	// now we have 8 memory (2 * 4 cells)
 	pok4 = Buffer.GetMemory(8); // error (nullptr)
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
 	pok4 = nullptr;
-
 	// marege free memory +
 	pok1 = Buffer.GetMemory(4); // XX XX 00 00 00
 	pok2 = Buffer.GetMemory(2); // XX XX XX 00 00
 	pok3 = Buffer.GetMemory(4); // XX XX XX XX XX
 	Buffer.ReturnMemory(pok1); // 00 00 XX XX XX
-	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00 
+	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00
 	Buffer.ReturnMemory(pok2); // 00 00-00-00 00
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
 	pok4 = nullptr;
-
 }
 */
 
@@ -134,45 +119,38 @@ void testFor_OPTadaC_MultithreadedSimpleMemoryBuffer()
 {
 	bool initDoneWithNoErrors_ = false;
 	OPTadaC_MultithreadedSimpleMemoryBuffer Buffer = OPTadaC_MultithreadedSimpleMemoryBuffer(10, 5, 2, initDoneWithNoErrors_);
-
 	cout << "initDoneWithNoErrors_ - " << initDoneWithNoErrors_ << endl;
 	cout << "test buffer - " << Buffer.TestBuffer() << endl;
 	cout << "All locked memory :" << Buffer.Get_AllModulesLockedMemory() << endl;
 	cout << "Memory size :" << Buffer.Get_BufferMemorySize() << endl;
 	cout << "Locked memory :" << Buffer.Get_LockedMemory() << endl;
-
 	void* pok1 = nullptr;
 	void* pok2 = nullptr;
 	void* pok3 = nullptr;
 	void* pok4 = nullptr;
 	void* pok5 = nullptr;
 	void* pok6 = nullptr;
-
 	// incorrect or specifick input
 	pok1 = Buffer.GetMemory(-1); // ! unsigned parameter
 	pok1 = Buffer.GetMemory(1000000); // fragmetation +0 (2b - fragmet cell)
 	pok1 = Buffer.GetMemory(1000001); // fragmetation +1 (2b - fragmet cell)
-
 	// incorrect reff
 	int a;
 	pok1 = &a;
 	Buffer.ReturnMemory(pok1);
 	pok1 = nullptr;
 	Buffer.ReturnMemory(pok1);
-
 	// try get memory errors
 	pok1 = Buffer.GetMemory(8);
 	// low memory
 	pok2 = Buffer.GetMemory(3); // we have 2 now
 	// have no enaght memory
 	pok3 = Buffer.GetMemory(11); // we have 10 max
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
-
 	// fragmentaion mamory error +
 	pok1 = Buffer.GetMemory(4); // XX XX 00 00 00
 	pok2 = Buffer.GetMemory(2); // XX XX XX 00 00
@@ -181,14 +159,12 @@ void testFor_OPTadaC_MultithreadedSimpleMemoryBuffer()
 	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00
 	// now we have 8 memory (2 * 4 cells)
 	pok4 = Buffer.GetMemory(8); // error (nullptr)
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
 	pok4 = nullptr;
-
 	// marege free memory +
 	pok1 = Buffer.GetMemory(4); // XX XX 00 00 00
 	pok2 = Buffer.GetMemory(2); // XX XX XX 00 00
@@ -196,14 +172,12 @@ void testFor_OPTadaC_MultithreadedSimpleMemoryBuffer()
 	Buffer.ReturnMemory(pok1); // 00 00 XX XX XX
 	Buffer.ReturnMemory(pok3); // 00 00 XX 00 00
 	Buffer.ReturnMemory(pok2); // 00 00-00-00 00
-
 	// refresh buffer
 	Buffer.Clear_Buffer();
 	pok1 = nullptr;
 	pok2 = nullptr;
 	pok3 = nullptr;
 	pok4 = nullptr;
-
 }
 */
 
@@ -218,87 +192,47 @@ void testFor_OPTada_MemoryManager()
 	void* point4 = nullptr;
 	void* point5 = nullptr;
 	void* point6 = nullptr;
-
 	OPTadaS_Key_MemoryManager* key1 = nullptr;
 	OPTadaS_Key_MemoryManager* key2 = nullptr;
 	OPTadaS_Key_MemoryManager* key3 = nullptr;
 	OPTadaS_Key_MemoryManager* key4 = nullptr;
 	OPTadaS_Key_MemoryManager* key5 = nullptr;
 	OPTadaS_Key_MemoryManager* key6 = nullptr;
-
 	OPTada_MemoryManager manager;
 	OPTada_MemoryManager manager2;
-
 	// init, free, new buffer, delete buffer, get memory, create key, check key
 	manager.Init_Mamager(3);
 	manager2.Init_Mamager(1);
-
 	key1 = manager.CreateNewMemoryBuffer(13, 500000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_MultithreadedSimpleMemoryBuffer);
 	key2 = manager.CreateNewMemoryBuffer(12, 500000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_SimpleMemoryBuffer);
 	key3 = manager.CreateNewMemoryBuffer(11, 500000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_MultithreadedSimpleMemoryBuffer);
-
 	key4 = manager2.CreateNewMemoryBuffer(11, 1000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_MultithreadedSimpleMemoryBuffer);
 	key5 = manager2.CreateNewMemoryBuffer(11, 1000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_MultithreadedSimpleMemoryBuffer);
 	manager2.DeleteMemoryBuffer(&key4);
 	key5 = manager2.CreateNewMemoryBuffer(11, 1000000, 5, 5, OPTadaE_BufferTypes_ForMemoryManager::ENUM_MultithreadedSimpleMemoryBuffer);
-
 	point1 = manager.GetMemory(key1, 10);
 	point1 = manager.GetMemory(key1, 100);
 	point1 = manager.GetMemory(key1, 1010);
-
 	manager.Clear_Buffer(key1);
-
 	manager.DeleteMemoryBuffer(&key4);
-
 	manager.DeleteMemoryBuffer(&key3);
 	manager.DeleteMemoryBuffer(&key2);
 	manager.Free_Manager();
-
 	manager.DeleteMemoryBuffer(&key1);
-
 	void* endpoint = nullptr;
 }
 */
 
 
 // class for testing
-class TestClass 
+class TestClass
 {
-<<<<<<< HEAD
-	//OPTada_C_CiclicBuffer_Memory asd(1); 
-	//OPTada_S_CiclicBufferElement *ff;
-
-	/*int re = sizeof(OPTada_S_CyclicBufferElement_Memory);
-	OPTada_C_CiclicBuffer_Memory asd(50003);
-
-	OPTada_S_CyclicBufferElement_Memory* one = nullptr;
-	OPTada_S_CyclicBufferElement_Memory* two = nullptr;
-	OPTada_S_CyclicBufferElement_Memory* tree = nullptr;
-	OPTada_S_CyclicBufferElement_Memory* fore = nullptr;
-
-
-
-
-
-
-	CRITICAL_SECTION ThreadSynchronization; // критическая секция для синхронизации потоков
-
-	InitializeCriticalSection(&ThreadSynchronization); // инициализация критической секции
-
-	DeleteCriticalSection(&ThreadSynchronization); // удаление критической секции
-
-	EnterCriticalSection(&ThreadSynchronization); // запрет на доступ (для синхронизации)
-
-	LeaveCriticalSection(&ThreadSynchronization); // разрешили доступ следующему потоку
-
-=======
 public:
->>>>>>> dev
 
 	int  i = 0;
 	char a = 's';
 
-	TestClass() 
+	TestClass()
 	{
 		i = 3;
 		a = 'd';
@@ -320,7 +254,7 @@ void timer()
 	OPTada_MemoryManager manager;
 	manager.Init_Mamager(3);
 	OPTadaS_Key_MemoryManager* key = manager.CreateNewMemoryBuffer(10, 100000000, 10000010, 8, OPTadaE_BufferTypes_ForMemoryManager::ENUM_SimpleMemoryBuffer);
-	cout << endl << "GetAllModulesLockedMemory memory managaer (in bytes) = " << manager.Get_AllModulesLockedMemory() <<  endl;
+	cout << endl << "GetAllModulesLockedMemory memory managaer (in bytes) = " << manager.Get_AllModulesLockedMemory() << endl;
 
 	TestClass* link = nullptr;
 
@@ -375,7 +309,7 @@ void main()
 	cout << " ---> MAIN enter point <---" << endl << endl;
 
 	// you can test manager here.
-	//timer();
+	timer();
 
 	cout << endl << "- - - - - - - - END - - - - - - - - -" << endl;
 }
